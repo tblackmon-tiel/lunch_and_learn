4d805edf2fad309432920e7743f4d34f4bf9d152
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-11-14 16:56:42 -0700

    add basic happy and sad path testing for user login

---
+++ spec/requests/api/v1/user_login_spec.rb
@@ -0,0 +1,55 @@
+require 'rails_helper'
+
+RSpec.describe "User login endpoint", type: :request do
+  describe "happy paths" do
+    it "returns a user details hash when a user successfully authenticates" do
+      User.create!(name: "Odell", email: "goodboy@ruffruff.com", password: "treats4lyf", api_key: "123456789abcd")
+      user_auth = {
+        "email": "goodboy@ruffruff.com",
+        "password": "treats4lyf"
+      }
+
+      post "/api/v1/sessions", params: user_auth, as: :json
+      expect(response).to be_successful
+
+      user = JSON.parse(response.body, symbolize_names: true)
+      expect(user).to be_a Hash
+      expect(user).to have_key(:data)
+      expect(user[:data]).to be_a Hash
+
+      data = user[:data]
+      expect(data).to have_key(:id)
+      expect(data[:id]).to be_a String
+      expect(data).to have_key(:type)
+      expect(data[:type]).to eq("user")
+      expect(data).to have_key(:attributes)
+      expect(data[:attributes]).to be_a Hash
+
+      attributes = data[:attributes]
+      expect(attributes).to have_key(:name)
+      expect(attributes[:name]).to be_a String
+      expect(attributes).to have_key(:email)
+      expect(attributes[:email]).to be_a String
+      expect(attributes).to have_key(:api_key)
+      expect(attributes[:api_key]).to be_a String
+    end
+  end
+
+  describe "sad paths" do
+    it "returns an error if the user cannot authenticate" do
+      User.create!(name: "Odell", email: "goodboy@ruffruff.com", password: "treats4lyf", api_key: "123456789abcd")
+      user_auth = {
+        "email": "goodboy@ruffruff.com",
+        "password": "treats4lyfeee"
+      }
+
+      post "/api/v1/sessions", params: user_auth, as: :json
+      expect(response).to_not be_successful
+
+      error = JSON.parse(response.body, symbolize_names: true)
+      expect(error).to be_a Hash
+      expect(error).to have_key(:errors)
+      expect(error[:errors]).to eq("")
+    end
+  end
+end
